f321f87643bd26ad44ff9def60bc59e3c51c6fd6
Marek <Marek.Myjak1221@gmail.com>
2019-10-01 22:09:58 +0200

    add SockMerchant solution

--- src/main/java/hackerrank/sockmerchant/SockMerchant.java
+++ src/main/java/hackerrank/sockmerchant/SockMerchant.java
@@ -1,4 +1,20 @@
 package hackerrank.sockmerchant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class SockMerchant {
+
+    static int sockMerchant(int n, int[] ar) {
+        Map<Integer, Integer> occurrencesMap = new HashMap<>();
+        for (int i : ar) {
+            occurrencesMap.computeIfPresent(i, (integer, integer2) -> ++integer2);
+            occurrencesMap.putIfAbsent(i, 1);
+        }
+        int result = 0;
+        for (Map.Entry<Integer, Integer> integerEntry : occurrencesMap.entrySet()) {
+            result += Math.floorDiv(integerEntry.getValue(), 2);
+        }
+        return result;
+    }
 }
--- src/test/java/hackerrank/sockmerchant/SockMerchantTest.java
+++ src/test/java/hackerrank/sockmerchant/SockMerchantTest.java
@@ -1,7 +1,26 @@
 package hackerrank.sockmerchant;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 class SockMerchantTest {
 
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(2, new int[]{1, 1}, 1),
+                Arguments.of(7, new int[]{1, 2, 1, 2, 1, 3, 2}, 2),
+                Arguments.of(9, new int[]{10, 20, 10, 10, 30, 50, 10, 20}, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    void sockMerchantTest(int n, int[] array, int numberOfMatchingPair) {
+        then(SockMerchant.sockMerchant(n, array)).isEqualTo(numberOfMatchingPair);
+    }
 }
